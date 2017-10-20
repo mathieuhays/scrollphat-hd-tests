06a4e00740953e86dc2164d04aee69d97b861fb9
Mathieu <mathieu@mathieuhays.com>
2017-10-20 22:47:47 +0100

    random brightness & faster animatino

--- iteration.py
+++ iteration.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
 import time
+from random import randrange
 import scrollphathd
 
 x = 0
 y = 0
 
 while True:
+    brightness = randrange(.05, 1, .05)
+
     scrollphathd.clear()
-    scrollphathd.set_pixel(x, y, .5)
+    scrollphathd.set_pixel(x, y, brightness)
     scrollphathd.show()
 
     x += 1
@@ -21,5 +24,5 @@ while True:
         x = 0
         y = 0
 
-    time.sleep(.5)
+    time.sleep(.05)
 
