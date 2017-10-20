56204f75215ada4a9a59a53c57edcb6712f4a750
Mathieu <mathieu@mathieuhays.com>
2017-10-21 00:31:15 +0100

    minimize magic numbers and add comments

--- pan.py
+++ pan.py
@@ -5,22 +5,28 @@ from random import randrange
 import math
 import scrollphathd
 
+DISPLAY_WIDTH = 17
+DISPLAY_HEIGHT = 7
+
 x = 0
 
 while True:
     scrollphathd.clear()
 
-    brightest_index = randrange(0, 7)
+    brightest_index = randrange(0, DISPLAY_HEIGHT)
 
-    for y in range(7):
+    for y in range(DISPLAY_HEIGHT):
         diff = brightest_index - y
-        brightness = 1 - (math.fabs(diff) / 3)  # 6 being the max number of leds remaining . 3 provide sharper fade
+        brightness = 1 - (math.fabs(diff) / ((DISPLAY_HEIGHT / 2) - 1))
 
+        # create greater contrast between main led and "fade" leds
         if diff != 0:
             brightness /= 2
 
+        # bring down overall brightness
         brightness /= 2
 
+        # avoid negative brightness
         if brightness < 0:
             brightness = 0
 
@@ -30,7 +36,7 @@ while True:
 
     x += 1
 
-    if x >= 17:
+    if x >= DISPLAY_WIDTH:
         x = 0
 
     time.sleep(.05)
